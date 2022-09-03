720bdeeaac905a860e09393af6a405b407ae92c2
kimnamsun <nsunny0908@naver.com>
2022-09-04 01:08:38 +0900

    :sparkles: model 작성

---
+++ src/Todo/model.ts
@@ -0,0 +1,28 @@
+import { ITodo } from './interface';
+
+/**
+ * @description Todo model
+ */
+class Todo implements ITodo {
+  id: number;
+  content: string;
+  isDone: boolean;
+  category: string;
+  tags?: string[];
+
+  constructor({ id, content, isDone, category, tags }: ITodo) {
+    this.id = id;
+    this.content = content;
+    this.isDone = isDone;
+    this.category = category;
+    this.tags = tags;
+  }
+}
+
+const todo = new Todo({
+  id: 1,
+  content: 'read book',
+  isDone: false,
+  category: 'study',
+  tags: ['0904'],
+});
